87e65d8dd4fa488067888cc8c4071cd0b80fe13d
oluvercetti <ayawori25000@gmail.com>
2025-01-06 15:45:56 +0100

    Refactor marketing.js to enhance navigation handling by allowing the use of a default history object. This change improves the flexibility of the mount function, enabling better integration with different routing strategies in the MarketingApp component.

--- marketing/src/marketing.js
+++ marketing/src/marketing.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import { createMemoryHistory } from 'history';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 
-const mount = (el, { onNavigate }) => {
-    const history = createMemoryHistory();
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
     if (onNavigate) {
         history.listen(onNavigate);
     }
@@ -23,7 +23,7 @@ const mount = (el, { onNavigate }) => {
 if (process.env.NODE_ENV === 'development') {
     const devRoot = document.querySelector('#_marketing-dev-root');
     if (devRoot) {
-        mount(devRoot);
+        mount(devRoot, { defaultHistory: createBrowserHistory() });
     }
 }
 
