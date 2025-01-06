416a8b83dc346b4da0c41850907b353163e143b0
oluvercetti <ayawori25000@gmail.com>
2025-01-06 15:38:53 +0100

    Enhance navigation handling in MarketingApp component by integrating history management and updating mount function in marketing.js. This allows for synchronized routing between the container and marketing applications, improving user experience during navigation.

--- marketing/src/marketing.js
+++ marketing/src/marketing.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom';
 import App from './App';
 import { createMemoryHistory } from 'history';
 
-const mount = (el) => {
+const mount = (el, { onNavigate }) => {
     const history = createMemoryHistory();
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
     ReactDOM.render(<App history={history} />, el);
+
+    return {
+        onParentNavigate: ({ pathname: nextPathname }) => {
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        },
+    };
 };
 
 if (process.env.NODE_ENV === 'development') {
